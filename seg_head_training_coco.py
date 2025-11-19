cce96180d38f075b1447d63381dcaa9fac123ff8
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-11-18 22:34:00 -0500

    coco

--- seg_head_training_coco.py
+++ seg_head_training_coco.py
@@ -264,7 +264,7 @@ def main():
 
     train_loader = DataLoader(
         train_dataset,
-        batch_size=4,
+        batch_size=1,  # <-- changed from 4 to 1
         shuffle=True,
         num_workers=4,
         pin_memory=True
