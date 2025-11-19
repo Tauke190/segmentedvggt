d0d7e6c364ebb19e1cfb7dd9bff12223f60f1cfb
Avinash Gyavali <avinash@Avinashs-MacBook-Air.local>
2025-11-18 19:14:56 -0500

    COCO traiing

--- coco.py
+++ coco.py
@@ -1,26 +1,89 @@
-from torchvision.datasets import CocoSegmentation
-from torch.utils.data import DataLoader
+import os
+import numpy as np
+from PIL import Image
 
-# Paths to your COCO images and annotation files
-train_img_dir = "dataset/train2017"
-train_ann_file = "dataset/annotations/instances_train2017.json"
+import torch
+from torch.utils.data import Dataset, DataLoader
+from pycocotools.coco import COCO
+import torchvision.transforms as T
 
-def coco_transform(image, target):
-    image = torchvision.transforms.ToTensor()(image)
-    target = torchvision.transforms.PILToTensor()(target).squeeze(0).long()
-    return image, target
 
-# Create the dataset
-train_dataset = CocoSegmentation(
-    root=train_img_dir,
-    annFile=train_ann_file,
-    transforms=coco_transform
-)
+# -----------------------------
+#  Transforms for image + mask
+# -----------------------------
+def coco_transform(image, mask):
+    image = T.ToTensor()(image)
+    mask = torch.from_numpy(np.array(mask)).long()
+    return image, mask
 
-# Create the dataloader
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=4,
-    shuffle=True,
-    num_workers=4
-)
+
+# ------------------------------------
+#   COCO Semantic Segmentation Dataset
+# ------------------------------------
+class COCOSegmentation(Dataset):
+    def __init__(self, img_dir, ann_file, transforms=None):
+        self.img_dir = img_dir
+        self.coco = COCO(ann_file)
+        self.ids = list(self.coco.imgs.keys())
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, index):
+        img_id = self.ids[index]
+
+        # Load image metadata
+        img_info = self.coco.loadImgs(img_id)[0]
+        img_path = os.path.join(self.img_dir, img_info["file_name"])
+        
+        # Load RGB image
+        image = Image.open(img_path).convert("RGB")
+
+        # Load all annotations for this image
+        ann_ids = self.coco.getAnnIds(imgIds=img_id, iscrowd=None)
+        anns = self.coco.loadAnns(ann_ids)
+
+        # Create empty mask (H x W)
+        mask = np.zeros((img_info["height"], img_info["width"]), dtype=np.uint8)
+
+        # Fill mask: category_id per pixel
+        for ann in anns:
+            m = self.coco.annToMask(ann)
+            mask = np.maximum(mask, m * ann["category_id"])
+
+        mask = Image.fromarray(mask)
+
+        # Apply transform
+        if self.transforms:
+            image, mask = self.transforms(image, mask)
+
+        return image, mask
+
+
+# ------------------------
+#  Create dataset + loader
+# ------------------------
+if __name__ == "__main__":
+    train_img_dir = "dataset/train2017"
+    train_ann_file = "dataset/annotations/instances_train2017.json"
+
+    train_dataset = COCOSegmentation(
+        img_dir=train_img_dir,
+        ann_file=train_ann_file,
+        transforms=coco_transform
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=4,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True
+    )
+
+    # Test loading one batch
+    for images, masks in train_loader:
+        print("Images batch:", images.shape)  # [B, 3, H, W]
+        print("Masks batch:", masks.shape)    # [B, H, W]
+        break
