25c981e2f40f590e36579d6a2f3080eed8610810
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-11-26 12:11:15 -0500

    fixed some stuffs

--- dataset/coco_dataset.py
+++ dataset/coco_dataset.py
@@ -6,13 +6,11 @@ from pycocotools.coco import COCO
 
 
 class COCOSegmentation(Dataset):
-    def __init__(self, img_dir, ann_file, transforms=None, return_instance_masks=False):
+    def __init__(self, img_dir, ann_file, transforms=None):
         self.img_dir = img_dir
         self.coco = COCO(ann_file)
         self.ids = list(self.coco.imgs.keys())
         self.transforms = transforms
-        self.return_instance_masks = return_instance_masks
-
         # Build category_id to contiguous index mapping
         cats = self.coco.loadCats(self.coco.getCatIds())
         self.cat_id_to_index = {cat['id']: i+1 for i, cat in enumerate(cats)}  # +1 to reserve 0 for background
@@ -31,43 +29,18 @@ class COCOSegmentation(Dataset):
 
         height, width = img_info["height"], img_info["width"]
 
-        if self.return_instance_masks:
-            # Return a stack of instance masks (N, H, W) and their category ids
-            instance_masks = []
-            category_ids = []
-            for ann in anns:
-                mask_instance = self.coco.annToMask(ann)
-                if mask_instance.max() > 0:
-                    instance_masks.append(mask_instance)
-                    category_ids.append(ann["category_id"])
-            if instance_masks:
-                instance_masks = np.stack(instance_masks, axis=0)  # (N, H, W)
-                category_ids = np.array(category_ids, dtype=np.int64)
-            else:
-                # No objects: return empty arrays
-                instance_masks = np.zeros((0, height, width), dtype=np.uint8)
-                category_ids = np.zeros((0,), dtype=np.int64)
-            mask = (instance_masks, category_ids)
-        else:
-            # Return a single mask (all objects combined)
-            mask = np.zeros((height, width), dtype=np.uint8)
-            for ann in anns:
-                mask_instance = self.coco.annToMask(ann)
-                mask = np.maximum(mask, mask_instance)
-            mask = Image.fromarray(mask)
+        # Return a single mask (all objects combined)
+        mask = np.zeros((height, width), dtype=np.uint8)
+        for ann in anns:
+            mask_instance = self.coco.annToMask(ann)
+            # Assign category index to mask pixels
+            cat_idx = self.cat_id_to_index[ann["category_id"]]
+            mask[mask_instance > 0] = cat_idx
+        mask = Image.fromarray(mask)
 
         if self.transforms:
-            if self.return_instance_masks:
-                # Only apply transforms to image; masks are numpy arrays
-                image = self.transforms(image)
-                # You may want to add mask transforms here if needed
-                return image, mask[0], mask[1]  # image, instance_masks, category_ids
-            else:
-                image, mask = self.transforms(image, mask)
-                return image, mask
-
-        if self.return_instance_masks:
-            return image, mask[0], mask[1]
-        else:
+            image, mask = self.transforms(image, mask)
             return image, mask
+
+        return image, mask
 
